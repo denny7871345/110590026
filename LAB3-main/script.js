40b17eafde0fe530635d9e0b1e492c01982fd785
denny7871345 <90140271+denny7871345@users.noreply.github.com>
2022-11-04 15:44:59 +0800

    Add files via upload

---
+++ LAB3-main/script.js
@@ -0,0 +1,33 @@
+function ChangeTheColorB(){
+  document.getElementById("mytable").style.backgroundColor="blue";
+}
+function ChangeTheColorG(){
+  document.getElementById("mytable").style.backgroundColor="green";
+}
+function ChangeTheColorR(){
+  document.getElementById("mytable").style.backgroundColor="red";
+}
+function ChangeTheColorO(){
+  document.getElementById("mytable").style.backgroundColor="orange";
+}
+function ChangeTheWidth200(){
+  document.getElementById("mytable").style.width="200px";
+}
+function ChangeTheWidth500(){
+  document.getElementById("mytable").style.width="500px";
+}
+function ChangeTheBorder1(){
+  document.getElementById("mytable").style.border="1px solid black";
+  document.getElementById("mytable").style.borderSpacing="1px";
+}
+function ChangeTheBorder10(){
+  document.getElementById("mytable").style.border="10px solid black";
+  document.getElementById("mytable").style.borderSpacing="10px";
+}
+function ChangeTheBorder20(){
+  document.getElementById("mytable").style.border="20px solid black";
+  document.getElementById("mytable").style.borderSpacing="20px";
+}
+function remove(){
+  document.getElementById("mytable").removeAttribute("style");
+}
